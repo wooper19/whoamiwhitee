0c15caa21b7aceef59779cbf33e41a39e0439672
wooper19 <tb388656@gmail.com>
2025-06-10 00:57:33 +0800

    ㄋ

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // 在這裡添加 'images' 屬性來禁用優化
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  export default nextConfig;
